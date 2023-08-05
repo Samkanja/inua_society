1be6510eb7cb85d20b5e08c15b32be1464f9ca1e
Samkanja <samuelkanja4@gmail.com>
2023-08-04 20:53:35 -0700

    anything

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -75,13 +75,9 @@ WSGI_APPLICATION = 'CoreRoot.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'inua_web',
-        'USER': 'core',
-        'PASSWORD': 'anjaksam',
-        'HOST': 'localhost',
-        'PORT': 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
